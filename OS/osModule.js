902c8884ec906419b4ab0337b69541c58d30fcf8
Rohit Kumar <rohitkumarmehra58@gmail.com>
2025-09-06 21:58:47 +0530

    os module path module

--- OS/osModule.js
+++ OS/osModule.js
@@ -32,4 +32,42 @@ const osModule = require('os');
 
 // console.log(osModule.version());
 
+// console.log(osModule.networkInterfaces());
+
+// Learning start from here 
+
+const os = require('os')
+
+// console.log(os)
+
+// const osArch = os.arch();
+// console.log(osArch)
+
+// const osPlatform = os.platform()
+// console.log(osPlatform)
+
+// const Fmem = os.freemem()
+// console.log(Fmem);
+
+// const TotalMem = os.totalmem()
+// console.log(((TotalMem/1024)/1024/1024))
+
+// const userInfo = os.userInfo();
+// console.log(userInfo)
+
+// const osType = os.type();
+// console.log(osType)
+
+// const homeDir = os.homedir();
+// console.log(homeDir)
+
+// const release = os.release()
+// console.log(release);
+
+// const Uptime = os.uptime();
+// console.log((Uptime / (60 * 60 * 24)))
+
+// console.log(osModule.cpus());
+// console.log(osModule.version());
+
 console.log(osModule.networkInterfaces());
--- Path/path.js
+++ Path/path.js
@@ -46,5 +46,45 @@ const path  = require('path');
 // console.log(relativePath);
 
 
-const isAbsoulte =  path.isAbsolute('D:\ROHIT KUMAR DATA\BACKEND\Node-Basic\Path\path.js`');
-console.log(isAbsoulte);
+// const isAbsoulte =  path.isAbsolute('D:\ROHIT KUMAR DATA\BACKEND\Node-Basic\Path\path.js`');
+// console.log(isAbsoulte);
+
+
+// Learning start from here 
+
+// for Checking the what wil be return the path 
+const pth = require('path')
+// console.log(pth);
+
+//  for checking the directory of the file
+// const pathDirName = pth.dirname('D:\ROHIT KUMAR DATA\BACKEND\Node-Basic\Path\path.js')
+// console.log(pathDirName) // o/p => D
+
+// for checking the filename 
+// const pathFileNameWithExt = pth.basename("D:\ROHIT KUMAR DATA\BACKEND\Node-Basic\Path\path.js")
+// console.log('path name extension is ',pathFileNameWithExt) // o/p =>  ROHIT KUMAR DATABACKENDNode-BasicPathpath.js
+
+// for checking the file extension 
+// const pathExtName = pth.extname ("D:\ROHIT KUMAR DATA\BACKEND\Node-Basic\Path\path.js");
+// console.log(pathExtName) // o/p => .js
+
+
+//  To Join two paths into one path
+// const joinedPath = pth.join('D:\ROHIT KUMAR DATA\BACKEND\Node-Basic\Path\path.js',  'D:\ROHIT KUMAR DATA\BACKEND\Node-Basic\Globals\global.js' );
+// console.log(joinedPath)
+
+// convert the pathe into an object
+// const parsePath = pth.parse('D:\ROHIT KUMAR DATA\BACKEND\Node-Basic\Globals\global.js');
+// // console.log(parsePath)
+
+// convert path object to string 
+// const formatPath = pth.format(parsePath)
+// console.log(formatPath)
+
+// checking the path is relative or not 
+// const relativePath = pth.relative('D:\ROHIT KUMAR DATA\BACKEND\Node-Basic\Globals\global.js', 'D:\ROHIT KUMAR DATA\BACKEND\Node-Basic\Path\path.js');
+// console.log('Relative path is ', relativePath)
+
+// checking the path is absolute or not 
+// const isAbsolute = path.isAbsolute('D:\ROHIT KUMAR DATA\BACKEND\Node-Basic\Path\path.js');
+// console.log(isAbsolute)
